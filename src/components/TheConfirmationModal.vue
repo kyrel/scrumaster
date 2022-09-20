5ec2b00088df31cec02aa04a4c1d533c3e28c04b
Mikhail Kirillov <mikhail.kirillov@outlook.com>
2022-09-20 21:09:53 +0300

    Focus trap for modals

--- src/components/TheConfirmationModal.vue
+++ src/components/TheConfirmationModal.vue
@@ -1,22 +1,25 @@
 <script setup lang="ts">
 import { useConfirmation } from '@/stores/confirmation';
+import { FocusTrap } from 'focus-trap-vue';
 import AppModalLayout from './AppModalLayout.vue';
 const confirmation = useConfirmation();
 </script>
 <template>
-    <AppModalLayout v-if="confirmation.opened" class="confirm-modal" @close="confirmation.opened = false">
-        <h3 class="confirm-modal__header">            
-            {{ confirmation.message }}            
-        </h3>
-        <div class="confirm-modal__controls">
-            <button class="btn" @click="confirmation.confirmClicked">
-                {{ confirmation.confirmButtonText }}
-            </button>
-            <button class="btn" @click="confirmation.cancelClicked">
-                {{ confirmation.cancelButtonText }}
-            </button>
-        </div>
-    </AppModalLayout>
+    <FocusTrap v-if="confirmation.opened">
+        <AppModalLayout v-if="confirmation.opened" class="confirm-modal" @close="confirmation.opened = false">
+            <h3 class="confirm-modal__header">            
+                {{ confirmation.message }}            
+            </h3>
+            <div class="confirm-modal__controls">
+                <button class="btn" @click="confirmation.confirmClicked">
+                    {{ confirmation.confirmButtonText }}
+                </button>
+                <button class="btn" @click="confirmation.cancelClicked">
+                    {{ confirmation.cancelButtonText }}
+                </button>
+            </div>
+        </AppModalLayout>
+    </FocusTrap>
 </template>
 <style scoped>
 /* @define confirm-modal */
